de214fc04f2d40b5bef1376ed8958ba62d52e270
Sly Kint A. Bacalso <64179667+Son0fAnton@users.noreply.github.com>
2021-08-20 02:37:58 +0800

    Update Config.class.php

--- classes/Config.class.php
+++ classes/Config.class.php
@@ -2,26 +2,10 @@
 
 interface Config{
     const SYSTEM_NAME = "KMB Solutions";
-    // const MySQLHost = "remotemysql.com"; 
-	// const MySQLDatabaseName = "XHx0hCpRsM";
-	// const MySQLUser = "XHx0hCpRsM";
-	// const MySQLPass = "qh9ZYMMsFw";
-
-	// const MySQLHost = "sql6.freemysqlhosting.net"; 
-	// const MySQLDatabaseName = "sql6430013";
-	// const MySQLUser = "sql6430013";
-	// const MySQLPass = "MuEKKLpTFw";
-
-	// const MySQLHost = "db4free.net"; 
-	// const MySQLDatabaseName = "kmbsolutions";
-	// const MySQLUser = "slygenius";
-	// const MySQLPass = "SlyTheGreat";
-    // const MySQLPort = 3306;
-
-	const MySQLHost = "localhost"; 
-	const MySQLDatabaseName = "kmb-solutions";
-	const MySQLUser = "root";
-	const MySQLPass = "SlyTheGreat";
+	const MySQLHost = ""; 
+	const MySQLDatabaseName = "";
+	const MySQLUser = "";
+	const MySQLPass = "";
     const MySQLPort = 3306;
 }
 ?>
